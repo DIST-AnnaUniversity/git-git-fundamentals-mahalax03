a54b13c4e4aed70b8ececcb40b91876ea75bd903
mahalax03 <125486038+mahalax03@users.noreply.github.com>
2023-02-17 11:44:09 +0530

    Add files via upload

---
+++ largestdigit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Enter a number"
+read num
+largest=0
+while [ $num -gt 0 ]
+do
+     temp=$(($num % 10))
+     if [ $temp -gt $largest ]
+     then
+          largest=$temp
+     fi
+     num=$(( $num/10))
+done
+
+echo "largest digit in the number is: $largest"
